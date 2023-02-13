3abe0bf10326f2c196c11086e2d48583a821d006
korg <omwitsa-etole@gmail.com>
2023-02-13 15:00:43 +0300

    3.3

--- rooms.py
+++ rooms.py
@@ -437,41 +437,41 @@ def ApiRoom(mode):
 			if user != None:
 				msg[0] = "false"
 				messages = []
-				if user != None:
-					try:
-						while True:
-							try:
-								db = connector()
-								break
-							except:
-								pass
-						cur = db.cursor(buffered=True)
-						cur.execute('update users set last_online=current_timestamp where email_id=%s and name=%s', (session["rooms-user"],session["room-name"],))
-						cur.execute('SELECT has_new FROM users WHERE email_id=%s and name=%s', (session["rooms-user"], user, ))
-						eid = cur.fetchone()
-						if eid:
-							if eid[0] == 1 or eid[0] == '1':
-								cur.execute('update users set has_new="0" where email_id=%s and name=%s', (session["rooms-user"], user,))
-								msg[0] = "true"
-						cur.execute("select on_call,caller_id from users where email_id=%s and name=%s",(session["rooms-user"],session["room-name"],))
-						calling = cur.fetchone()
-						if calling:
-							if calling[0] == 1 or calling[0] =="1":
-								cur.execute('update users set on_call="0" where email_id=%s and name=%s', (session["rooms-user"], user,))
-								if session.get("on-cal") != True:
-									
-									msg[1] ="true"
-									cur.execute("select name from users where key_ID=%s",(calling[1],))
-									msg[2] = cur.fetchone()[0]
-									session["active"] = msg[2]
-					except Exception as e:
-						print("here-")
-						db.rollback()
-						print(str(e))
-						pass
-					finally:
-						db.commit()
-						db.close()
+				
+				try:
+					while True:
+						try:
+							db = connector()
+							break
+						except:
+							pass
+					cur = db.cursor(buffered=True)
+					cur.execute('update users set last_online=current_timestamp where email_id=%s and name=%s', (session["rooms-user"],session["room-name"],))
+					cur.execute('SELECT has_new FROM users WHERE email_id=%s and name=%s', (session["rooms-user"], user, ))
+					eid = cur.fetchone()
+					if eid[0] != None:
+						if eid[0] == 1 or eid[0] == '1':
+							cur.execute('update users set has_new="0" where email_id=%s and name=%s', (session["rooms-user"], user,))
+							msg[0] = "true"
+					cur.execute("select on_call,caller_id from users where email_id=%s and name=%s",(session["rooms-user"],session["room-name"],))
+					calling = cur.fetchone()
+					if calling[0] != None:
+						if calling[0] == 1 or calling[0] =="1":
+							cur.execute('update users set on_call="0" where email_id=%s and name=%s', (session["rooms-user"], user,))
+							if session.get("on-cal") != True:
+								
+								msg[1] ="true"
+								cur.execute("select name from users where key_ID=%s",(calling[1],))
+								msg[2] = cur.fetchone()[0]
+								session["active"] = msg[2]
+				except Exception as e:
+					print("here-")
+					db.rollback()
+					print(str(e))
+					pass
+				finally:
+					db.commit()
+					db.close()
 			"""
 			if active != None:
 				from_user = session["room-name"]
@@ -530,6 +530,7 @@ def ApiRoom(mode):
 					db.commit()
 					db.close()
 			"""
+		print(msg[0]+","+msg[1]+","+msg[2])
 				
 		return msg[0]+","+msg[1]+","+msg[2]
 	if mode == "checkMessage":
